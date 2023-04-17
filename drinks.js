6d91c13692a7547b0a4b8fef51bc29f4cf41f2d9
trajkovskip <peroutest@gmail.com>
2023-04-17 18:07:25 +0200

    Initial commit

---
+++ drinks.js
@@ -0,0 +1,18 @@
+// Get the QR code data from the URL
+const urlParams = new URLSearchParams(window.location.search);
+const qrCodeData = urlParams.get('data');
+
+// Parse the JSON data
+const formData = JSON.parse(qrCodeData);
+
+// Get the drinks list element
+const drinksList = document.getElementById('drinks-list');
+
+// Loop through the form data and add each drink to the list
+for (const [drink, quantity] of Object.entries(formData)) {
+    if (quantity > 0) {
+        const drinkItem = document.createElement('li');
+        drinkItem.innerHTML = `<h2>${drink}</h2><p>Quantity: ${quantity}</p>`;
+        drinksList.appendChild(drinkItem);
+    }
+}
